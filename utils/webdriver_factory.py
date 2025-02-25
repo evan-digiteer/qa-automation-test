32fee288bb04ddfe70b7b124ccd9c8d7f623a895
evan-digiteer <marc@digiteer.digital>
2025-02-25 14:16:11 +0800

    Add cache clearing method and optimize WebDriver initialization in WebDriverFactory

--- utils/webdriver_factory.py
+++ utils/webdriver_factory.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import logging
 from functools import lru_cache
+from time import time
 
 class WebDriverFactory:
     @classmethod
@@ -10,6 +10,11 @@ class WebDriverFactory:
     def _get_driver_path(cls):
         """Cache the ChromeDriver path to avoid repeated downloads"""
         return ChromeDriverManager().install()
+
+    @staticmethod
+    def clear_cache():
+        """Clear the driver path cache to force new download"""
+        WebDriverFactory._get_driver_path.cache_clear()
 
     @classmethod
     def get_driver(cls):
@@ -22,13 +27,13 @@ class WebDriverFactory:
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--ignore-certificate-errors')
-            chrome_options.add_argument('--disable-gpu')  # Reduce GPU usage
-            chrome_options.add_argument('--disable-extensions')  # Disable extensions
-            chrome_options.add_argument('--disable-infobars')  # Disable infobars
-            chrome_options.add_argument('--disable-notifications')  # Disable notifications
-            chrome_options.add_argument('--disable-default-apps')  # Disable default apps
-            chrome_options.add_argument('--dns-prefetch-disable')  # Disable DNS prefetching
-            chrome_options.add_argument('--page-load-strategy=eager')  # Don't wait for full page load
+            chrome_options.add_argument('--disable-gpu')  
+            chrome_options.add_argument('--disable-extensions')  
+            chrome_options.add_argument('--disable-infobars')  
+            chrome_options.add_argument('--disable-notifications')  
+            chrome_options.add_argument('--disable-default-apps')  
+            chrome_options.add_argument('--dns-prefetch-disable')  
+            chrome_options.add_argument('--page-load-strategy=eager')  
             
             # Logging optimizations
             chrome_options.add_argument('--log-level=3')
@@ -40,18 +45,13 @@ class WebDriverFactory:
                 'profile.default_content_settings.popups': 0,
                 'profile.default_content_setting_values.automatic_downloads': 1,
                 'profile.default_content_setting_values.geolocation': 2,
-                'profile.managed_default_content_settings.images': 1,  # 2 to disable images
+                'profile.managed_default_content_settings.images': 1,  
                 'profile.default_content_setting_values.cookies': 1
             })
             
-            service = Service(
-                cls._get_driver_path(),
-                log_path='NUL'
-            )
-            
-            driver = webdriver.Chrome(service=service, options=chrome_options)
-            driver.set_page_load_timeout(30)  # Set page load timeout
-            driver.implicitly_wait(5)  # Set implicit wait
+            driver = webdriver.Chrome(options=chrome_options)
+            driver.set_page_load_timeout(30)  
+            driver.implicitly_wait(5) 
             
             return driver
 
